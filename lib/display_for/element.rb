91295530b7ab009bd5d958f946105d0c7f1d3677
Mateo Murphy <mateo.murphy@gmail.com>
2012-05-27 22:48:19 -0400

    Autoload html element

--- lib/display_for/element.rb
+++ lib/display_for/element.rb
@@ -3,5 +3,6 @@ module DisplayFor
     autoload :Action,     'display_for/element/action'
     autoload :Attribute,  'display_for/element/attribute'
     autoload :Base,       'display_for/element/base'
+    autoload :Html,       'display_for/element/html'
   end
 end
