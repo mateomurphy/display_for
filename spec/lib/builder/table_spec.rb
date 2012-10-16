d4177cd17e2cf17d0eb9710636a8ee2e9a0481ea
Mateo Murphy <mateo.murphy@gmail.com>
2012-10-16 12:27:42 -0400

    Added a simple spec for testing footer

---
+++ spec/lib/builder/table_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+OUTPUT = %q|<table class="table table-bordered table-striped test_model-table"><thead><tr><th class="test_model_id">id</th><th class="test_model_first_name">first_name</th><th class="test_model_last_name">last_name</th></tr></thead>
+<tbody><tr id="test_model_1"><td>1</td><td>foo</td><td>bar</td></tr>
+</tbody><tfoot><tr><td colspan="3">&nbsp;</td></tr>
+</tfoot></table>|
+
+module DisplayFor
+  module Builder
+    describe Table, :focus => true do 
+      context 'when given a footer' do
+        subject :table do
+          Table.new(TestModel, [TestModel.new(1, 'foo', 'bar')], {}, TestTemplate.new) do |t|
+            t.attribute :id
+            t.attribute :first_name
+            t.attribute :last_name
+            t.footer do |f|
+              f.html :form, :html => { :colspan => 3 } do
+                '&nbsp;'.html_safe
+              end
+            end
+          end
+        end
+
+        describe '#to_s' do
+          it 'renders the table' do
+            table.to_s.should eq(OUTPUT)
+          end
+        end
+      end
+    end
+  end
+end
